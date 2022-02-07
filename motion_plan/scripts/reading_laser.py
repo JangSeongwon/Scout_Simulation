e6a14f8800bd0c82b062bc02e0577c1a4c0dd074
Jang <87692888+JangSeongwon@users.noreply.github.com>
2022-02-07 12:25:34 +0900

    Update reading_laser.py

--- motion_plan/scripts/reading_laser.py
+++ motion_plan/scripts/reading_laser.py
@@ -2,20 +2,20 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 
-def clbk_laser(msg):
-    # 720/5 = 144
-    regions = [
-        min(msg.ranges[0:143]),
-        min(msg.ranges[144:287]),
-        min(msg.ranges[288:431]),
-        min(msg.ranges[432:575]),
-        min(msg.ranges[576:713]),
+def lidar_read(msg):
+
+    detection = [
+        round(min(msg.ranges[0:143]),2),
+        round(min(msg.ranges[144:287]),2),
+        round(min(msg.ranges[288:431]),2),
+        round(min(msg.ranges[432:575]),2),
+        round(min(msg.ranges[576:713]),2),
     ]
-    rospy.loginfo(regions)
+    rospy.loginfo(detection)
 
 def main():
     rospy.init_node('reading_laser')
-    sub= rospy.Subscriber("/scout/laser/scan", LaserScan, clbk_laser)
+    sub= rospy.Subscriber("/scout/laser/scan", LaserScan, lidar_read)
 
     rospy.spin()
 
